659517b18e0368357b36b3b090cb250fe4a5dc60
GeromeStartanimation <gerome.startanimation@gmail.com>
2025-09-04 11:02:37 +0800

    Updated DB Uri

--- TCGApi/app.js
+++ TCGApi/app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 
 const { MongoClient } = require("mongodb");
 // Replace the uri string with your connection string
-const uri = "mongodb://localhost:27017/";
+const uri = process.env.DB || "mongodb://localhost:27017/";
 const client = new MongoClient(uri);
 
 const database = client.db('tcg');
